2eeca511b5bfc88f3833e4f7a80235c4900cb2e0
tolik <tol.kowalev@gmail.com>
2025-10-18 17:53:47 +0300

    Remove unused var

--- src/components/shared/motion/bt-slide/bt-slide.tsx
+++ src/components/shared/motion/bt-slide/bt-slide.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC, PropsWithChildren, useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { motion } from "framer-motion";
 
 import { ElProps } from "@/types/common";
 
