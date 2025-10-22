3d523256082d9f4db5a8c1ed75be3a82f6678291
tolik <tol.kowalev@gmail.com>
2025-10-22 08:46:31 +0300

    Remove unused  var

--- src/components/home-page/why-deepnspire/why-deepnspire.tsx
+++ src/components/home-page/why-deepnspire/why-deepnspire.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 import Image from "next/image";
 import { motion, useInView } from "framer-motion";
 
