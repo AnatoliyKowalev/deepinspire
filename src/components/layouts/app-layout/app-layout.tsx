3f796db31fb31567b255e11d7ed4b7d9eb2e4faa
tolik <tol.kowalev@gmail.com>
2025-10-22 18:52:15 +0300

    Hide footer

--- src/components/layouts/app-layout/app-layout.tsx
+++ src/components/layouts/app-layout/app-layout.tsx
@@ -1,13 +1,13 @@
 import React, { FC, PropsWithChildren } from "react";
 
-import Footer from "./footer";
+// import Footer from "./footer";
 import Header from "./header";
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => (
   <main className="grid relative">
     <Header />
     {children}
-    <Footer />
+    {/* <Footer /> */}
   </main>
 );
 
