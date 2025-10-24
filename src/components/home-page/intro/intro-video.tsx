3353b1b42fba9eca5358f4df98506ad4829fbc7f
tolik <tol.kowalev@gmail.com>
2025-10-24 16:10:26 +0300

    Fix intro

--- src/components/home-page/intro/intro-video.tsx
+++ src/components/home-page/intro/intro-video.tsx
@@ -3,9 +3,9 @@ import { FC } from "react";
 import SourceVideo from "@/components/shared/source-video";
 
 const IntroVideo: FC = () => (
-  <div className="absolute w-full bottom-0 right-0 overflow-hidden pb-[80px] mb-[-80px] xl:h-fit xl:max-w-[1000px] 1xl:max-w-[1500px] xl:top-0 pointer-events-none">
+  <div className="absolute w-full bottom-0 right-0 overflow-hidden pb-25 -mb-25 xl:w-325 h-fit pointer-events-none">
     <SourceVideo
-      className="max-w-none ml-auto w-100 xs:w-125 transform translate-y-20 xs:translate-x-4 xs:translate-y-18 md:translate-y-24 md:translate-x-6 md:w-225 lg:w-250 lg:translate-x-8 xl:translate-y-10 xl:translate-x-50 1xl:w-375 1xl:translate-x-80 1xl:-translate-y-4"
+      className="max-w-none ml-auto w-full translate-x-10 translate-y-10 xs:w-120A transform xs:translate-y-14 md:translate-y-24 md:translate-x-6 md:w-225 lg:w-250 lg:translate-x-20 xl:translate-y-25 xl:translate-x-35 1xl:w-375 1xl:translate-x-16 max-h-[110vh]"
       webm="/video/home/intro.webm"
       mp4="/video/home/intro-safari.mp4"
       aria-hidden="true"
--- src/components/home-page/intro/intro.tsx
+++ src/components/home-page/intro/intro.tsx
@@ -7,7 +7,7 @@ import Icon from "@/components/shared/icon";
 import { INTRO_LINKS, INTRO_LIST } from "./constants";
 
 const Intro: FC = () => (
-  <section className="h-screen h-187.5 xs:h-208 bg-accent-dark pt-39 xs:pt-31 md:pt-39 sxs:pr-8 font-montserrat xs:pl-15 1xl:pt-54 md:h-[1050px] xl:max-h-[650px] 1xl:h-[970px] relative">
+  <section className="h-screen h-187.5 xs:h-208 bg-accent-dark pt-39 xs:pt-31 md:pt-39 sxs:pr-8 font-montserrat xs:pl-15 1xl:pt-54 md:h-[1050px] xl:h-[650px] 1xl:h-[970px] relative">
     <div className="relative container h-full text-light-texting font-medium flex flex-col gap-4 xs:gap-5 xl:gap-12 xs:text-base xl:pb-15 1xl:justify-between 1xl:text-xl z-2">
       <h1 className="font-bold relative text-[28px]/normal xs:text-[30px]/normal xl:text-3xl max-w-147.5 xl:max-w-110.5 1xl:text-4xl 1xl:max-w-147.5 xl:leading-normal">
         Boutique software development company that bridges business and
