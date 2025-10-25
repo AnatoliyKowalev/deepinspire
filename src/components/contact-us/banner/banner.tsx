faf2c13880f0bd1a6d4c1dce9fffa83126d27262
tolik <tol.kowalev@gmail.com>
2025-10-25 11:04:59 +0300

    Fix banner position

--- src/components/contact-us/banner/banner.tsx
+++ src/components/contact-us/banner/banner.tsx
@@ -5,7 +5,7 @@ const Banner: FC = () => (
   <section className="h-60 xl:h-77.5">
     <Image
       src="/img/contact-us/top_contactus.jpg"
-      className="object-cover object-center h-full w-full -z-10"
+      className="object-cover h-full w-full -z-10 object-[19%] xs:object-[22%] md:object-[30%] lg:object-center"
       width={2000}
       height={500}
       quality={100}
